b4902e40b9b80e1733dc0edfd2d3eefed4891738
hohserg <hohserg1@yandex.ru>
2019-01-08 02:50:35 +0300

    Added main class and test

---
+++ src/main/java/hohserg/jhocon/Config.java
@@ -0,0 +1,11 @@
+package hohserg.jhocon;
+
+public class Config {
+    public int test1 = 1;
+    public String test2 = "test";
+
+    @Override
+    public String toString() {
+        return test1 + " " + test2;
+    }
+}
---
+++ src/main/java/hohserg/jhocon/Main.java
@@ -0,0 +1,52 @@
+package hohserg.jhocon;
+
+import com.github.dahaka934.jhocon.JHocon;
+import com.github.dahaka934.jhocon.JHoconBuilder;
+import net.minecraftforge.fml.common.Mod;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.function.Supplier;
+
+@Mod(modid = Main.MODID, name = Main.NAME, version = Main.VERSION)
+public class Main {
+    public static final String MODID = "jhocon";
+    public static final String NAME = "JHocon";
+    public static final String VERSION = "1.0";
+    private static JHocon jhocon = new JHoconBuilder()
+            .withComments()
+            .registerDefaultValidators()
+            .throwErrorOnValidationFail(true)
+            .create();
+
+    public static void main(String[] args) {
+        Config c = getOrCreateConig("test", Config::new);
+        System.out.println(c);
+
+    }
+
+    public static <Config> Config getOrCreateConig(String modid, Supplier<Config> defaultConfig) {
+        String configFile = "./config/" + modid + ".cfg";
+        Config config = defaultConfig.get();
+        try {
+            String lines = Files.readAllLines(Paths.get(configFile), StandardCharsets.UTF_8)
+                    .stream()
+                    .reduce("", (a, b) -> a + b + "\n");
+
+            config = jhocon.<Config>fromHocon(lines, "config", config.getClass());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            String hocon = jhocon.toHocon("config", config.getClass());
+            try {
+                Files.write(Paths.get(configFile), hocon.getBytes(StandardCharsets.UTF_8));
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+        return config;
+    }
+}
